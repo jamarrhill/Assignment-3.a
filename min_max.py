058dead3323334f0bf9e79171805c332500fb73e
jamarrhill <hilljama@oregonstate.edu>
2020-10-13 02:10:09 -0800

    Assignment 3.a

---
+++ min_max.py
@@ -0,0 +1,11 @@
+print("How many integers would you like to enter?")
+var = int(input())
+print("Please enter", var ,"integers.")
+while var != int(input()):
+    print("input integer:")
+
+
+
+
+
+
