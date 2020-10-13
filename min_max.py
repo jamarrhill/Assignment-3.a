ab75f685b9e55986d7c34c301af92737d519c39e
jamarrhill <hilljama@oregonstate.edu>
2020-10-13 02:14:52 -0800

    Assignment 3.a

--- min_max.py
+++ min_max.py
@@ -1,3 +1,7 @@
+# Name: Jamar Hill
+# Date: 10/13/2020
+# Description: Assignment 3.a
+
 print("How many integers would you like to enter?")
 var = int(input())
 print("Please enter", var ,"integers.")
